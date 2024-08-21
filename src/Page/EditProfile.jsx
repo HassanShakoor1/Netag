de2ca0429ed60bfafd3393ef906c296575e03702
HassanShakoor1 <140912131+HassanShakoor1@users.noreply.github.com>
2024-08-21 19:10:07 +0500

    cmt

--- src/Page/EditProfile.jsx
+++ src/Page/EditProfile.jsx
@@ -1,6 +1,5 @@
-// EditProfile.js
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import the hook
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IoChevronBack } from "react-icons/io5";
 import edit from '../images/edit.png';
 import editcircle from '../images/editcircle.png';
@@ -9,17 +8,88 @@ import '../App.css';
 import nav from '../images/nav-img.png';
 import { TextField } from '@mui/material';
 import { styled } from '@mui/system';
+import { ref, set } from "firebase/database";
+import { database } from '../firebase';
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const CustomTextField = styled(TextField)({
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '1rem',
+    width: "100%",
+    paddingLeft: "20px",
+  },
+});
 
 function EditProfile() {
-  const navigate = useNavigate(); // Use the hook here
-
-  const CustomTextField = styled(TextField)({
-    '& .MuiOutlinedInput-root': {
-      borderRadius: '1rem',
-      width: "100%",
-      paddinLeft: "20px",
-    },
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: '',
+    designation: '',
+    status: '',
+    company: '',
+    nickname: '',
+    ladyImgUrl: '',
+    mainImgUrl: ''
   });
+
+  const [files, setFiles] = useState({
+    ladyImg: null,
+    mainImg: null
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = async (e) => {
+    const { id } = e.target;
+    const file = e.target.files[0];
+    if (file) {
+      if (id === 'lady-img-upload') {
+        setFiles((prevFiles) => ({ ...prevFiles, ladyImg: file }));
+        const ladyImgUrl = await uploadImage(file);
+        setFormData((prevData) => ({ ...prevData, ladyImgUrl }));
+      } else if (id === 'main-img-upload') {
+        setFiles((prevFiles) => ({ ...prevFiles, mainImg: file }));
+        const mainImgUrl = await uploadImage(file);
+        setFormData((prevData) => ({ ...prevData, mainImgUrl }));
+      }
+    }
+  };
+
+  const uploadImage = async (file) => {
+    const storage = getStorage();
+    const storageReference = storageRef(storage, `images/${Date.now()}_${file.name}`);
+    await uploadBytes(storageReference, file);
+    return getDownloadURL(storageReference);
+  };
+
+  const handleSave = async () => {
+    try {
+      const userId = Date.now().toString();
+      const userRef = ref(database, `User/${userId}`);
+
+      const updatedData = {
+        ...formData,
+        ladyImgUrl: files.ladyImg ? formData.ladyImgUrl : formData.ladyImgUrl,
+        mainImgUrl: files.mainImg ? formData.mainImgUrl : formData.mainImgUrl
+      };
+
+      await set(userRef, updatedData);
+      alert("Data saved successfully!");
+
+      localStorage.setItem('userId', userId);
+      localStorage.setItem('profileData', JSON.stringify(updatedData));
+
+      navigate('/home'); // Redirect to profile page after saving
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
+  };
 
   const handleBack = () => {
     navigate(-1);
@@ -37,15 +107,92 @@ function EditProfile() {
           </div>
         </nav>
 
-        {/* navvv end... */}
-
         <div className="rel-div" style={{ flexDirection: "column" }}>
-          <div className='lady' style={{ top: "131px", backgroundColor: "#D9D9D9", border: '1px solid grey' }}>
-            <img style={{ display: "flex", justifyContent: 'center', alignItems: 'center', margin: "20px auto", width: "50px" }} src={editcircle} alt="lady" />
+          <div className='lady' style={ladyStyle}>
+            {formData.ladyImgUrl ? (
+              <img className='main-img' style={{width:'100%'}}  src={formData.ladyImgUrl} alt="Uploaded Lady Image" />
+            ) : (
+              <img style={imgStyle} src={editcircle} alt="Upload Icon" />
+            )}
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: 'none' }}
+              id="lady-img-upload"
+              onChange={handleFileChange}
+            />
+            {!formData.ladyImgUrl && (
+              <label
+                htmlFor="lady-img-upload"
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '60px',
+                  fontSize: '10px',
+                  marginTop: '8px',
+                  color: '#4A5568',
+                  height: '27px',
+                  borderRadius: '4px',
+                  cursor: 'pointer',
+                  textAlign: 'center',
+                  padding: '0px',
+                  margin: '2px auto'
+                }}
+              >
+                Upload Photos
+              </label>
+            )}
           </div>
           <div>
-            <div className='main-img' style={{ width: "100%", height: '300px', backgroundColor: "#D9D9D9" }}>
-              <img style={{ display: "flex", justifyContent: 'center', alignItems: 'center', margin: "20px auto", paddingTop: "3rem", width: "70px" }} src={edit} alt="lady" />
+            <div className='main-img' style={mainImgStyle}>
+              {formData.mainImgUrl ? (
+                <img style={{
+                 
+                objectFit:'cover',
+                  width: '100%',
+               height:'-webkit-fill-available',
+           
+                }} src={formData.mainImgUrl} alt="Uploaded Main Image" />
+              ) : (
+                <img style={{
+                  display: "flex",
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  margin: "20px auto",
+                  width: "100px",
+                  marginTop: '30px'
+                }} src={edit} alt="Upload Icon" />
+              )}
+              <input
+                type="file"
+                accept="image/*"
+                style={{ display: 'none' }}
+                id="main-img-upload"
+                onChange={handleFileChange}
+              />
+              {!formData.mainImgUrl && (
+                <label
+                  htmlFor="main-img-upload"
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    border: '1px solid #e2e8f0',
+                    width: '110px',
+                    fontSize: '10px',
+                    marginTop: '8px',
+                    color: '#4A5568',
+                    height: '27px',
+                    borderRadius: '4px',
+                    cursor: 'pointer',
+                    textAlign: 'center',
+                    margin: '20px auto'
+                  }}
+                >
+                  Upload Photos
+                </label>
+              )}
             </div>
           </div>
         </div>
@@ -56,58 +203,83 @@ function EditProfile() {
           <div className="edit-field">
             <CustomTextField
               label="UserName"
-              id="outlined-size-small"
-              defaultValue=""
-              size="small"
-            />
-
-            <CustomTextField
-              label="Nickname"
-              id="outlined-size-small"
-              defaultValue=""
-              size="small"
-            />
-          </div>
-
-          <div className="edit-field">
-            <CustomTextField
-              label="UserName"
-              id="outlined-size-small"
-              defaultValue=""
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
               size="small"
             />
 
             <CustomTextField
               label="Designation"
-              id="outlined-size-small"
-              defaultValue=""
+              name="designation"
+              value={formData.designation}
+              onChange={handleChange}
               size="small"
             />
           </div>
 
           <div className="edit-field">
             <CustomTextField
-              label="Material status"
-              id="outlined-size-small"
-              defaultValue=""
+              label="Material Status"
+              name="status"
+              value={formData.status}
+              onChange={handleChange}
               size="small"
             />
 
             <CustomTextField
               label="Company"
-              id="outlined-size-small"
-              defaultValue=""
+              name="company"
+              value={formData.company}
+              onChange={handleChange}
               size="small"
             />
           </div>
+          <div className="edit-field">
+            <CustomTextField
+              label="Nickname"
+              name="nickname"
+              value={formData.nickname}
+              onChange={handleChange}
+              size="small"
+            />
+          </div>
+
           <br /><br /><br /><br />
           <div className="btn-s">
-            <button style={{ color: 'white',fontSize:"20px",width:"92%" }} className='save2'>Save</button>
+            <button style={saveButtonStyle} className='save2' onClick={handleSave}>Save</button>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const ladyStyle = {
+  top: "131px",
+  backgroundColor: "#D9D9D9",
+  border: '1px solid grey'
+};
+
+const imgStyle = {
+  display: "flex",
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: "2px auto",
+  width: "60px"
+};
+
+const mainImgStyle = {
+  width: "100%",
+  height: '300px',
+  backgroundColor: "#D9D9D9",
+  marginTop: '3rem'
+};
+
+const saveButtonStyle = {
+  color: 'white',
+  fontSize: "20px",
+  width: "92%"
+};
 
 export default EditProfile;
