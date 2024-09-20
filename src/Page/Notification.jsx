3cff32ff1b316e2e2998ca12d3ee2f7f3eb130ae
HassanShakoor1 <140912131+HassanShakoor1@users.noreply.github.com>
2024-09-20 15:48:39 +0500

    notifications added

--- src/Page/Notification.jsx
+++ src/Page/Notification.jsx
@@ -1,8 +1,48 @@
 import "./notification.css"
 import vector from "../images/vector.png"
 import { useNavigate } from "react-router-dom"
+import { useState,useEffect } from "react";
+import { ref, get, query, orderByChild, equalTo } from "firebase/database";
+import { database } from '../firebase';  // Ensure correct path
+import { Query, orderBy } from "firebase/firestore";
 function Notification(){
     const navigate = useNavigate();
+
+const [data,setData]=useState([])
+const userId=localStorage.getItem('userId')
+useEffect(() => {
+
+    const getData= async()=>{
+        try{
+        const dataRef= query(
+            ref(database,`Notifications`),
+            orderByChild('parentid'),
+            equalTo(userId)
+        )
+        const snap=await get(dataRef)
+        const data=await snap.val()
+        console.log(data)
+        
+        const filterData=Object.keys(data).map(key=>({
+            id:key,
+            ...data[key]
+        }))
+        console.log(filterData)
+        setData(filterData)
+      
+  
+    }catch(errer){
+        console.log(errer)
+    }
+   
+    }
+    
+getData();
+
+}, [])
+
+
+
 
     const goback=()=>{
       navigate(-1);
@@ -26,91 +66,27 @@ function Notification(){
 
                         {/* All Notifications */}
 
+
                         <div style={{display:"flex",padding:"16px",marginTop:"2rem",fontSize:"14px",fontWeight:"500"}}>
                         All Notifications
                         </div>
 
                         {/* box1 */}
 
-                        <div className="notification-boxwidth">
-                            <div style={{color:"#EE0000",fontWeight:"500"}}>
-                            You have new order
-                            </div>
-                            <div style={{fontSize:"13px",fontWeight:"500"}}>
-                            12:30 - 12/12/2023
-                            </div>
-                            <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
-                                Lorem ipsum, dolor sit amet consectetur  elit.   rerum quaerat vel suscipit adipisci voluptate repellat aut officia maxime!
-                            </div>
-
-                        </div>
-                        {/* box2 */}
-                        <div style={{marginTop:"1rem"}} className="notification-boxwidth">
-                            <div style={{color:"#EE0000",fontWeight:"500"}}>
-                            Somebody visit your profile
-                            </div>
-                            <div style={{fontSize:"13px",fontWeight:"500"}}>
-                            12:30 - 12/12/2023
-                            </div>
-                            <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
-                                Lorem ipsum, dolor sit amet consectetur  elit.   rerum quaerat vel suscipit adipisci voluptate repellat aut officia maxime!
-                            </div>
-
-                        </div>
-                        {/* box3 */}
-                        <div style={{marginTop:"1rem",backgroundColor:"#FCFCFC"}} className="notification-boxwidth">
-                            <div style={{color:"#EE0000",fontWeight:"500"}}>
-                            You have new order
-                            </div>
-                            <div style={{fontSize:"13px",fontWeight:"500"}}>
-                            12:30 - 12/12/2023
-                            </div>
-                            <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
-                                Lorem ipsum, dolor sit amet consectetur  elit.   rerum quaerat vel suscipit adipisci voluptate repellat aut officia maxime!
-                            </div>
-
-                        </div>
-                        {/* box4 */}
-                        <div style={{marginTop:"1rem",backgroundColor:"#FCFCFC"}} className="notification-boxwidth">
-                            <div style={{color:"#EE0000",fontWeight:"500"}}>
-                            Somebody visit your profile
-                            </div>
-                            <div style={{fontSize:"13px",fontWeight:"500"}}>
-                            12:30 - 12/12/2023
-                            </div>
-                            <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
-                                Lorem ipsum, dolor sit amet consectetur  elit.   rerum quaerat vel suscipit adipisci voluptate repellat aut officia maxime!
-                            </div>
-
-                        </div>
-                        {/* box5 */}
-                        <div style={{marginTop:"1rem",backgroundColor:"#FCFCFC"}} className="notification-boxwidth">
-                            <div style={{color:"#EE0000",fontWeight:"500"}}>
-                            You have new order
-                            </div>
-                            <div style={{fontSize:"13px",fontWeight:"500"}}>
-                            12:30 - 12/12/2023
-                            </div>
-                            <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
-                                Lorem ipsum, dolor sit amet consectetur  elit.   rerum quaerat vel suscipit adipisci voluptate repellat aut officia maxime!
-                            </div>
-
-                        </div>
-                        {/* box6 */}
-                        <div style={{marginTop:"1rem",backgroundColor:"#FCFCFC"}} className="notification-boxwidth">
-                            <div style={{color:"#EE0000",fontWeight:"500"}}>
-                            Somebody visit your profile
-                            </div>
-                            <div style={{fontSize:"13px",fontWeight:"500"}}>
-                            12:30 - 12/12/2023
-                            </div>
-                            <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
-                                Lorem ipsum, dolor sit amet consectetur  elit.   rerum quaerat vel suscipit adipisci voluptate repellat aut officia maxime!
-                            </div>
-
-                        </div>
-
-
+                        {data && data.length > 0 && data.map((item) => (
+    <div key={item.id} className="notification-boxwidth">
+        <div style={{color:"#EE0000",fontWeight:"500"}}>
+            {item.title}
+        </div>
+        <div style={{fontSize:"13px",fontWeight:"500"}}>
+           {item.createdat}
+        </div>
+        <div style={{color:"#777777",fontSize:"12px",width:"90%"}}>
+{item.body}
+        </div>
+    </div>
+))}
+ 
 
                     </div>
 
